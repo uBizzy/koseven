f0d0c8ccf17407068d90c9f987765bcc88f59852
nzpetter <pgolasz@gmail.com>
2018-07-17 20:43:37 +0200

    New default Config

--- modules/encrypt/config/encrypt.php
+++ modules/encrypt/config/encrypt.php
@@ -1,18 +1,29 @@
 <?php
-
+/**
+ * The following options must be set:
+ *
+ * string   key     secret passphrase
+ */
 return [
-
-	'default' => [
-		'type' => 'mcrypt',
-		/**
-		 * The following options must be set:
-		 *
-		 * string   key     secret passphrase
-		 * integer  mode    encryption mode, one of MCRYPT_MODE_*
-		 * integer  cipher  encryption cipher, one of the Mcrpyt cipher constants
-		 */
-		'cipher' => MCRYPT_RIJNDAEL_128,
-		'mode' => MCRYPT_MODE_NOFB,
-	],
-
+    'default' => [
+        'type' => Kohana_Encrypt_Engine_Openssl::TYPE,
+        'key' => NULL
+    ],
+//    'sodium' => [
+//        'type' => Kohana_Encrypt_Engine_Sodium::TYPE,
+//        'key' => NULL
+//    ],
+//    /**
+//     * Mcrypt is deprecated and should not be used,
+//     * however it requires additional options:
+//     *
+//     * integer  mode    encryption mode, one of MCRYPT_MODE_*
+//     * integer  cipher  encryption cipher, one of the Mcrypt cipher constants
+//     */
+//    'mcrypt' => [
+//        'type' => Kohana_Encrypt_Engine_Mcrypt::TYPE,
+//        'cipher' => MCRYPT_RIJNDAEL_128,
+//        'mode' => MCRYPT_MODE_CBC,
+//        'key' => NULL
+//    ]
 ];
