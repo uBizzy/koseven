bd7d966d0507d19aa4a133148e94d31b02790e7c
Tobias Oitzinger <tobiasoitzinger@gmail.com>
2019-01-04 02:13:01 +0100

    Fixed unit test and added KAT test for all supported ciphers

--- modules/encrypt/tests/EncryptSodiumTest.php
+++ modules/encrypt/tests/EncryptSodiumTest.php
@@ -1,7 +1,5 @@
 <?php
-
-use Encrypt_Engine_Sodium as Sodium;
-
+include_once 'EncryptTestBase.php';
 /**
  * @group      kohana
  * @group      kohana.encrypt
@@ -9,7 +7,7 @@ use Encrypt_Engine_Sodium as Sodium;
  * @package    Kohana/Encrypt
  * @category   Test
  * @author     Koseven Team
- * @copyright  (c) Kohana Team
+ * @copyright  (c) Koseven Team
  * @license    https://koseven.ga/LICENSE.md
  */
 class EncryptSodiumTest extends EncryptTestBase
@@ -33,25 +31,12 @@ class EncryptSodiumTest extends EncryptTestBase
     }
 
     /**
-     * @dataProvider provider_encode_and_decode
-     * @param string $plaintext
-     * @return void
-     */
-    public function test_256_bit(string $plaintext)
-    {
-        $this->set_config([
-            Sodium::CONFIG_TYPE => Sodium::TYPE,
-            Sodium::CONFIG_KEY => EncryptTestBase::KEY32,
-        ]);
-
-        $this->test_encode_and_decode($plaintext);
-    }
-
-    /**
      * Tests against known answers
      *
      * @dataProvider provider_kat
      *
+	 * @covers Kohana_Encrypt_Engine_Sodium::encrypt
+	 *
      * @param  array $vectors Known Answers
      * @throws Kohana_Exception
      */
@@ -59,20 +44,37 @@ class EncryptSodiumTest extends EncryptTestBase
     {
         // Init
         extract($vectors);
+
         $this->set_config([
-            Sodium::CONFIG_TYPE => Sodium::TYPE,
-            Sodium::CONFIG_KEY => EncryptTestBase::KEY32,
-        ]);
+        	'type'	 => 'sodium',
+			'key'	 => EncryptTestBase::KEY32,
+			'cipher' => $cipher
+		]);
 
         // Test encryption with known answers
-        /** @var Encrypt $encrypt */
         $encrypt = Encrypt::instance();
         $encrypted = $encrypt->encode($plaintext, $iv);
 
-        $this->assertEquals($encrypted, $ciphertext);
+        $this->assertEquals($ciphertext, $encrypted);
         $decrypted = $encrypt->decode($encrypted);
         $this->assertEquals($decrypted, $plaintext);
     }
+
+	/**
+	 * Tests decrypt function with invalid ciphertext
+	 */
+    public function test_decrypt_invalid()
+	{
+    	// Init
+		$this->set_config([
+			'type'	 => 'sodium',
+			'key'	 => EncryptTestBase::KEY32
+		]);
+
+		$encrypt = Encrypt::instance();
+		$this->assertNull($encrypt->decode(':/invalid?1'));
+		$this->assertNull($encrypt->decode(base64_encode('asdasd')));
+	}
 
     /**
      * Data source for kat
@@ -85,29 +87,98 @@ class EncryptSodiumTest extends EncryptTestBase
                 [
                     'iv' => '000000000000',
                     'ciphertext' => 'eyJpdiI6Ik1EQXdNREF3TURBd01EQXciLCJ2YWx1ZSI6Inp2MncyTmdQWW5TcFdBclV2Y2xsblVTVzFQMD0ifQ==',
-                    'plaintext' => 'test'
-                ],
-                [
-                    'iv' => '111111111111',
-                    'ciphertext' => 'eyJpdiI6Ik1URXhNVEV4TVRFeE1URXgiLCJ2YWx1ZSI6ImxSVXgwWDBNQklkSXRUOGw5cGIwVmtMSm96XC9GIn0=',
-                    'plaintext' => 'test2'
-                ],
-                [
-                    'iv' => '222222222222',
-                    'ciphertext' => 'eyJpdiI6Ik1qSXlNakl5TWpJeU1qSXkiLCJ2YWx1ZSI6Ik5OWGxETGNmZmlhKzlZd2xOSDFvSzZCd1wvRmlBIn0=',
-                    'plaintext' => 'test3'
-                ],
-                [
-                    'iv' => '333333333333',
-                    'ciphertext' => 'eyJpdiI6Ik16TXpNek16TXpNek16TXoiLCJ2YWx1ZSI6IlJrMnlXTXRPRGlrcG8yckNsTFVpTG93RDZWODEifQ==',
-                    'plaintext' => 'test4'
-                ],
-                [
-                    'iv' => '444444444444',
-                    'ciphertext' => 'eyJpdiI6Ik5EUTBORFEwTkRRME5EUTAiLCJ2YWx1ZSI6IitkT2JsRkd0eTFLZkpaSU0zYjN4T2UremlKbEkifQ==',
-                    'plaintext' => 'test5'
-                ],
-            ]
+                    'plaintext'  => 'test',
+					'cipher'	 => Encrypt_Engine_Sodium::AES_256_GCM
+                ]
+            ],
+			[
+				[
+					'iv' => '111111111111',
+					'ciphertext' => 'eyJpdiI6Ik1URXhNVEV4TVRFeE1URXgiLCJ2YWx1ZSI6ImxSVXgwWDBNQklkSXRUOGw5cGIwVmtMSm96XC9GIn0=',
+					'plaintext' => 'test2',
+					'cipher'	 => NULL
+				]
+			],
+			[
+				[
+					'iv' => '222222222222',
+					'ciphertext' => 'eyJpdiI6Ik1qSXlNakl5TWpJeU1qSXkiLCJ2YWx1ZSI6Ik5OWGxETGNmZmlhKzlZd2xOSDFvSzZCd1wvRmlBIn0=',
+					'plaintext' => 'test3',
+					'cipher'	 => NULL
+				]
+			],
+			[
+				[
+					'iv' => '33333333',
+					'ciphertext' => 'eyJpdiI6Ik16TXpNek16TXpNPSIsInZhbHVlIjoibGZBYjZ3eGdISGZmQ2o2aWduSE1sWlg5UEQ4aSJ9',
+					'plaintext' => 'test4',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY
+				]
+			],
+			[
+				[
+					'iv' => '44444444',
+					'ciphertext' => 'eyJpdiI6Ik5EUTBORFEwTkRRPSIsInZhbHVlIjoiZ2xaVXRMaFJEN29KK2RLU3pxdjA4RlpwdFVNcyJ9',
+					'plaintext' => 'test5',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY
+				]
+			],
+			[
+				[
+					'iv' => '55555555',
+					'ciphertext' => 'eyJpdiI6Ik5UVTFOVFUxTlRVPSIsInZhbHVlIjoiYTJNbjNXWFdRbVNhbnZuXC9XeTFQeHM3ZHJvXC9qIn0=',
+					'plaintext' => 'test6',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY
+				]
+			],
+			[
+				[
+					'iv' => '666666666666',
+					'ciphertext' => 'eyJpdiI6Ik5qWTJOalkyTmpZMk5qWTIiLCJ2YWx1ZSI6IldNcDVFdXVZTGZuM3F1SFlBaE5jZlVGTkxcL1IxIn0=',
+					'plaintext' => 'test7',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY_IETF
+				]
+			],
+			[
+				[
+					'iv' => '777777777777',
+					'ciphertext' => 'eyJpdiI6Ik56YzNOemMzTnpjM056YzMiLCJ2YWx1ZSI6ImFxd0ZMM1ZBXC8rUjI0Vzk4aHRTbkhOS29ibEE5In0=',
+					'plaintext' => 'test8',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY_IETF
+				]
+			],
+			[
+				[
+					'iv' => '888888888888',
+					'ciphertext' => 'eyJpdiI6Ik9EZzRPRGc0T0RnNE9EZzQiLCJ2YWx1ZSI6InZGR0Q0Nnhac0NmMWlnQWd0dnM5cXpVZjA2eFoifQ==',
+					'plaintext' => 'test9',
+					'cipher'	 => Encrypt_Engine_Sodium::CHACHA_POLY_IETF
+				]
+			],
+			[
+				[
+					'iv' => '999999999999999999999999',
+					'ciphertext' => 'eyJpdiI6Ik9UazVPVGs1T1RrNU9UazVPVGs1T1RrNU9UazVPVGs1IiwidmFsdWUiOiJkVXRrZ3lzUG9SN3FMblhveU5mdTlPeG5uT3puOVE9PSJ9',
+					'plaintext' => 'test10',
+					'cipher'	 => Encrypt_Engine_Sodium::XCHACHA_POLY_IETF
+				]
+			],
+			[
+				[
+					'iv' => '000000000000000000000000',
+					'ciphertext' => 'eyJpdiI6Ik1EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3IiwidmFsdWUiOiJmUyt3dzBQdENkUDlRZ1ZxeW1yN2Y4dkg0ZFlsMlE9PSJ9',
+					'plaintext' => 'test11',
+					'cipher'	 => Encrypt_Engine_Sodium::XCHACHA_POLY_IETF
+				]
+			],
+			[
+				[
+					'iv' => '111111111111111111111111',
+					'ciphertext' => 'eyJpdiI6Ik1URXhNVEV4TVRFeE1URXhNVEV4TVRFeE1URXhNVEV4IiwidmFsdWUiOiJPMU9oalE5UmVVdXRqM0pXbEhIWVp2aWN2SmtjTEE9PSJ9',
+					'plaintext' => 'test12',
+					'cipher'	 => Encrypt_Engine_Sodium::XCHACHA_POLY_IETF
+				]
+			]
         ];
     }
 }
