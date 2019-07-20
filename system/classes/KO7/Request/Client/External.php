121239afc9b529e4daf41a00494023163f5dccf8
Tobias Oitzinger <tobiasoitzinger@gmail.com>
2019-07-20 18:19:14 +0200

    Check if instanceof Request_Client

--- system/classes/KO7/Request/Client/External.php
+++ system/classes/KO7/Request/Client/External.php
@@ -101,8 +101,8 @@ abstract class KO7_Request_Client_External extends Request_Client {
 
 		$client = new $client($options);
 
-		// Check if client extends Request_Client_External
-		if( ! $client instanceof Request_Client_External)
+		// Check if client extends Request_Client
+		if( ! $client instanceof Request_Client)
 		{
 			throw new Request_Exception('Selected client is not a valid Request Client.');
 		}
