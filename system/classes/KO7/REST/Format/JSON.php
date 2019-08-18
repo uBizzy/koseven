2a01bf3be9aadd447cd71997257ea8c6888fdd17
Tobias Oitzinger <tobiasoitzinger@gmail.com>
2019-08-19 00:30:23 +0200

    JSON add exception handling

--- system/classes/KO7/REST/Format/JSON.php
+++ system/classes/KO7/REST/Format/JSON.php
@@ -12,11 +12,47 @@ class KO7_REST_Format_JSON extends REST_Format {
     /**
      * Format function
      *
+     * @throws REST_Exception
+     *
      * @return string
      */
     public function format() : string
     {
-        return json_encode($this->_body);
+        try
+        {
+            $body = json_encode($this->_body,
+                JSON_THROW_ON_ERROR
+                |JSON_FORCE_OBJECT
+                |JSON_NUMERIC_CHECK
+                |JSON_PRESERVE_ZERO_FRACTION
+                |JSON_UNESCAPED_UNICODE
+            );
+        }
+        catch(Exception $e)
+        {
+            $message = 'Unknown JSON error.';
+
+            switch(json_last_error()) {
+                case JSON_ERROR_DEPTH:
+                    $message = 'Maximum JSON stack depth has been exceeded.';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $message = 'JSON is not properly formed or invalid.';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $message = 'Error in JSON control characters. This usually happens with incorrect encoding.';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $message = 'Syntax error - Invalid JSON.';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $message = 'Malformed JSON UTF-8 characters. This usually happens with incorrect encoding.';
+                    break;
+            }
+            throw new REST_Exception($message, NULL, $e->getCode(), $e);
+        }
+
+        return $body;
     }
 
 }
