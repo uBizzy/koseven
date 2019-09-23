3b7745d4a643e26c355c9621d13dba2ebd3669cb
Tobias Oitzinger <tobiasoitzinger@gmail.com>
2019-09-23 17:27:55 +0200

    Change path construction in HTML formatter

--- system/classes/KO7/REST/Format/HTML.php
+++ system/classes/KO7/REST/Format/HTML.php
@@ -18,29 +18,26 @@ class KO7_REST_Format_HTML extends REST_Format {
      */
     public function format() : string
     {
-        // Parse Request Directory
-        $directory = strtolower($this->_request->directory());
+        // Filter the path parts, remove empty ones
+        $path = array_filter([
+            $this->_request->directory(),
+            $this->_request->controller(),
+            $this->_request->action(),
+        ]);
 
-        // Prepend a directory separator if a directory is set
-        if ($directory)
-        {
-            $directory .= DIRECTORY_SEPARATOR;
-        }
-
-        // Determine the view name
-        $view_name = $directory.strtolower($this->_request->controller());
+        $path = strtolower(implode(DIRECTORY_SEPARATOR, $path));
 
         // Try to find view, change view name if not found
         // e.g Controller: Welcome, check views/Welcome.php else views/welcome/{index,update,create,delete}.php
-        if (KO7::find_file('views', $view_name) === FALSE)
+        if (KO7::find_file('views', dirname($path)))
         {
-            $view_name .= DIRECTORY_SEPARATOR.$this->_request->action();
+            $path = dirname($path);
         }
 
         // Try to initialize View
         try
         {
-            return View::factory($view_name,  $this->_body)->render();
+            return View::factory($path,  $this->_body)->render();
         }
         catch (View_Exception $e)
         {
